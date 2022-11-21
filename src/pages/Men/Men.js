1c564925205387bffefe67179bafab05f3178fc2
EgorZapatylak <zapatylak96a@gmail.com>
2022-11-21 23:36:26 +0300

    Added some changed in Men components

--- src/pages/Men/Men.js
+++ src/pages/Men/Men.js
@@ -1,105 +1,108 @@
 import React from 'react';
 import './Men.css';
+import {Link} from "react-router-dom";
 
 function men() {
     return (
         <section>
-        <div className='men_header'>
-            <div className='road'>
-                <p>Home</p>
-                <div className='ul'/>
-                <span>Men</span>
+            <div className='men_header'>
+                <div className='road'>
+                    <p>Home</p>
+                    <div className='ul'/>
+                    <span>Men</span>
+                </div>
+                <div className='share'>
+                    <div className='share_img'/>
+                    <p>Share</p>
+                </div>
             </div>
-            <div className='share'>
-                <div className='share_img'/>
-                <p>Share</p>
+            <div className='title'>
+                <h1>Men</h1>
             </div>
-        </div>
-        <div className='title'>
-            <h1>Men</h1>
-        </div>
-        <div className='edit'>
-            <div className='filter'>
-            <div className='filter_img'/>
-            <p>Filter</p>
+            <div className='edit'>
+                <div className='filter'>
+                    <div className='filter_img'/>
+                    <p>Filter</p>
+                </div>
+                <div className='veia'>
+                    <div className='tabl'/>
+                    <div className='plit'/>
+                </div>
+                <div className='categor'>
+                    <p>BESTSELLERS</p>
+                    <div className='vector'/>
+                </div>
             </div>
-            <div className='veia'>
-            <div className='tabl'/>
-            <div className='plit'/>
+            <div className='items'>
+                <Link to="/product">
+                    <div className='clothes'>
+                        <div className='mid_1'></div>
+                        <p>Men's tracksuit Q109</p>
+                        <div className='cost-rate'>
+                            <p>$30.00</p>
+                            <div className='stars'></div>
+                        </div>
+                    </div>
+                </Link>
+                <div className='clothes'>
+                    <div className='mid_2'></div>
+                    <p>Men's tracksuit Q109</p>
+                    <div className='cost-rate'>
+                        <p>$30.00</p>
+                        <div className='stars'></div>
+                    </div>
+                </div>
+                <div className='clothes'>
+                    <div className='mid_3'></div>
+                    <p>Men's tracksuit Q109</p>
+                    <div className='cost-rate'>
+                        <p>$30.00</p>
+                        <div className='stars'></div>
+                    </div>
+                </div>
+                <div className='clothes'>
+                    <div className='mid_4'></div>
+                    <p>Men's tracksuit Q109</p>
+                    <div className='cost-rate'>
+                        <p>$30.00</p>
+                        <div className='stars'></div>
+                    </div>
+                </div>
+                <div className='clothes'>
+                    <div className='mid_5'></div>
+                    <p>Men's tracksuit Q109</p>
+                    <div className='cost-rate'>
+                        <p>$30.00</p>
+                        <div className='stars'></div>
+                    </div>
+                </div>
+                <div className='clothes'>
+                    <div className='mid_6'></div>
+                    <p>Men's tracksuit Q109</p>
+                    <div className='cost-rate'>
+                        <p>$30.00</p>
+                        <div className='stars'></div>
+                    </div>
+                </div>
+                <div className='clothes'>
+                    <div className='mid_7'></div>
+                    <p>Men's tracksuit Q109</p>
+                    <div className='cost-rate'>
+                        <p>$30.00</p>
+                        <div className='stars'></div>
+                    </div>
+                </div>
+                <div className='clothes'>
+                    <div className='mid_8'></div>
+                    <p>Men's tracksuit Q109</p>
+                    <div className='cost-rate'>
+                        <p>$30.00</p>
+                        <div className='stars'></div>
+                    </div>
+                </div>
             </div>
-            <div className='categor'>
-                <p>BESTSELLERS</p>
-                <div className='vector'/>
+            <div className='loading'>
             </div>
-        </div>
-        <div className='items'>
-        <div className='clothes'>
-                    <div className='id_1'></div>
-                    <p>Women's tracksuit Q109</p>
-                    <div className='cost-rate'>
-                        <p>$30.00</p>
-                        <div className='stars'></div>
-                    </div>
-                </div>
-                <div className='clothes'>
-                    <div className='id_2'></div>
-                    <p>Women's tracksuit Q109</p>
-                    <div className='cost-rate'>
-                        <p>$30.00</p>
-                        <div className='stars'></div>
-                    </div>
-                </div>
-                <div className='clothes'>
-                    <div className='id_3'></div>
-                    <p>Women's tracksuit Q109</p>
-                    <div className='cost-rate'>
-                        <p>$30.00</p>
-                        <div className='stars'></div>
-                    </div>
-                </div>
-                <div className='clothes'>
-                    <div className='id_4'></div>
-                    <p>Women's tracksuit Q109</p>
-                    <div className='cost-rate'>
-                        <p>$30.00</p>
-                        <div className='stars'></div>
-                    </div>
-                </div>
-                <div className='clothes'>
-                    <div className='id_5'></div>
-                    <p>Women's tracksuit Q109</p>
-                    <div className='cost-rate'>
-                        <p>$30.00</p>
-                        <div className='stars'></div>
-                    </div>
-                </div>
-                <div className='clothes'>
-                    <div className='id_6'></div>
-                    <p>Women's tracksuit Q109</p>
-                    <div className='cost-rate'>
-                        <p>$30.00</p>
-                        <div className='stars'></div>
-                    </div>
-                </div>
-                <div className='clothes'>
-                    <div className='id_7'></div>
-                    <p>Women's tracksuit Q109</p>
-                    <div className='cost-rate'>
-                        <p>$30.00</p>
-                        <div className='stars'></div>
-                    </div>
-                </div>
-                <div className='clothes'>
-                    <div className='id_8'></div>
-                    <p>Women's tracksuit Q109</p>
-                    <div className='cost-rate'>
-                        <p>$30.00</p>
-                        <div className='stars'></div>
-                    </div>
-                </div>
-        </div>
-        <div className='loading'>
-        </div>
         </section>
     )
 }
