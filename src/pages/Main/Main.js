5711620e1e4e8af12a88b0ea9ba495c24509c174
EgorZapatylak <zapatylak96a@gmail.com>
2022-12-05 21:24:06 +0300

    Some fixed in Main components

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Main.css';
 // import {Swiper} from 'swiper/react'
-import Card from '../Main/img/gray.svg';
 import {Link} from "react-router-dom";
 import {Hero} from "../../components/Hero/Hero";
 import {Advantage} from "../../components/Advantage/Advantage";
 import {Promice} from "../../components/Promice/Promice";
-import Offer, {SpecialOffer} from "../../components/SpecialOffer/SpecialOffer";
+import {SpecialOffer} from "../../components/SpecialOffer/SpecialOffer";
 import {Blog} from "../../components/Blog/Blog";
+import {Products} from "../../components/Products/Products";
 
 function main() {
     return (
@@ -15,132 +15,7 @@ function main() {
             <Hero/>
             <Advantage/>
             <div className='line'></div> {/*need sink about it =)*/}
-            <div className='item_hed'>
-                <div className='item_info'>
-                    <h2>Women's</h2>
-                </div>
-                <div className='sort'>
-                    <h3>New Arrivals</h3>
-                    <h3>Specials</h3>
-                    <h3>Bestseller</h3>
-                    <h3>Most Viewed</h3>
-                    <h3>Featured Products</h3>
-                </div>
-            </div>
-            <div className='items'>
-                <Link to="/product">
-                    <div className='clothes'>
-                        <div className='women_1'></div>
-                        <div className='clothes_info'>
-                            <p>Women's tracksuit Q109</p>
-                            <div className='cost-rate'>
-                                <p>$30.00</p>
-                                <div className='stars'></div>
-                            </div>
-                            <div className='clothes_event'>
-                                <div className="clothes_info_img">
-                                        <img width={40} height={40} src={Card} alt='blue'/>
-                                        <img width={40} height={40} src={Card} alt='white'/>
-                                        <img width={40} height={40} src={Card} alt='black'/>
-                                        <img width={40} height={40} src={Card} alt='grey'/>
-                                </div>
-                                <div className='clothes_info_size'>
-                                    <div className='xs'>
-                                        <p>XS</p>
-                                    </div>
-                                    <div className='s'>
-                                        <p>S</p>
-                                    </div>
-                                    <div className='m'>
-                                        <p>M</p>
-                                    </div>
-                                    <div className='l'>
-                                        <p>L</p>
-                                    </div>
-                                </div>
-                                <div className='clothes_info_event'>
-                                    <button>ADD TO CART</button>
-                                    <div className="heart_1"></div>
-                                    <div className="scale_1"></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </Link>
-                <Link to="/product">
-                <div className='clothes'>
-                    <div className='women_2'></div>
-                    <p>Women's tracksuit Q109</p>
-                    <div className='cost-rate'>
-                        <p>$30.00</p>
-                        <div className='stars'></div>
-                    </div>
-                </div>
-                </Link>
-                <Link to="/product">
-                <div className='clothes'>
-                    <div className='women_3'></div>
-                    <p>Women's tracksuit Q109</p>
-                    <div className='cost-rate'>
-                        <p>$30.00</p>
-                        <div className='stars'></div>
-                    </div>
-                </div>
-                </Link>
-                <Link to="/product">
-                <div className='clothes'>
-                    <div className='women_4'></div>
-                    <p>Women's tracksuit Q109</p>
-                    <div className='cost-rate'>
-                        <p>$30.00</p>
-                        <div className='stars'></div>
-                    </div>
-                </div>
-                </Link>
-                <Link to="/product">
-                <div className='clothes'>
-                    <div className='women_5'></div>
-                    <p>Women's tracksuit Q109</p>
-                    <div className='cost-rate'>
-                        <p>$30.00</p>
-                        <div className='stars'></div>
-                    </div>
-                </div>
-                </Link>
-                <Link to="/product">
-                <div className='clothes'>
-                    <div className='women_6'></div>
-                    <p>Women's tracksuit Q109</p>
-                    <div className='cost-rate'>
-                        <p>$30.00</p>
-                        <div className='stars'></div>
-                    </div>
-                </div>
-                </Link>
-                <Link to="/product">
-                <div className='clothes'>
-                    <div className='women_7'></div>
-                    <p>Women's tracksuit Q109</p>
-                    <div className='cost-rate'>
-                        <p>$30.00</p>
-                        <div className='stars'></div>
-                    </div>
-                </div>
-                </Link>
-                <Link to="/product">
-                <div className='clothes'>
-                    <div className='women_8'></div>
-                    <p>Women's tracksuit Q109</p>
-                    <div className='cost-rate'>
-                        <p>$30.00</p>
-                        <div className='stars'></div>
-                    </div>
-                </div>
-                </Link>
-                <Link to="/women">
-                    <button className='women_button'>See All</button>
-                </Link>
-            </div>
+            <Products title="Women's" />
             <div className='item_hed'>
                 <div className='item_info'>
                     <h2>Men's</h2>
