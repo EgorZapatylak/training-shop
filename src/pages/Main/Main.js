93b027968dc05bdd37e47ecbaa945987702602d7
EgorZapatylak <zapatylak96a@gmail.com>
2022-12-05 18:46:52 +0300

    Some fixed in Main components

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -7,6 +7,7 @@ import {Hero} from "../../components/Hero/Hero";
 import {Advantage} from "../../components/Advantage/Advantage";
 import {Promice} from "../../components/Promice/Promice";
 import Offer, {SpecialOffer} from "../../components/SpecialOffer/SpecialOffer";
+import {Blog} from "../../components/Blog/Blog";
 
 function main() {
     return (
@@ -239,42 +240,7 @@ function main() {
             </div>
             <Promice />
             <SpecialOffer />
-            <div className='blog_header'>
-                <h2>LATEST FROM BLOG</h2>
-                <p>SEE ALL</p>
-            </div>
-            <div className='blog_post'>
-                <div className='box_1'>
-                    <div className='box_info'>
-                        <h3>The Easiest Way to Break</h3>
-                        <p>But I must explain to you how all this mistaken idea of denouncing pleas <br/> and praising
-                            pain was bor</p>
-                        <div className='data'>
-                            <p>April 6, 2022<span>Read More</span></p>
-                        </div>
-                    </div>
-                </div>
-                <div className='box_2'>
-                    <div className='box_info'>
-                        <h3>Wedding Season</h3>
-                        <p>But I must explain to you how all this mistaken idea of denouncing pleas <br/> and praising
-                            pain was bor</p>
-                        <div className='data'>
-                            <p>April 6, 2022<span>Read More</span></p>
-                        </div>
-                    </div>
-                </div>
-                <div className='box_3'>
-                    <div className='box_info'>
-                        <h3>Recent Favorites On Repeat</h3>
-                        <p>But I must explain to you how all this mistaken idea of denouncing pleas <br/> and praising
-                            pain was bor</p>
-                        <div className='data'>
-                            <p>April 6, 2022<span>Read More</span></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Blog />
         </section>
     )
 }
