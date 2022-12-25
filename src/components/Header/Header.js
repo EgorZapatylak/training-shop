6531947613b8ba2125c28c19b1997788241c101b
EgorZapatylak <zapatylak96a@gmail.com>
2022-12-26 00:40:44 +0300

    Added "Burger-menu"for screens in 992 pixels on Hero components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -19,32 +19,32 @@ function header() {
             <div className="top_bar">
                 <div className='info'>
                     <div className='phone'>
-                        <img src ={Phone} alt =''/>
+                        <img src={Phone} alt=''/>
                         <p>+375 29 100 20 30</p>
                     </div>
                     <div className='location'>
-                        <img src ={Location} alt =''/>
+                        <img src={Location} alt=''/>
                         <p>Belarus, Gomel, Lange 17</p>
                     </div>
                     <div className='time'>
-                        <img src ={Clock} alt =''/>
+                        <img src={Clock} alt=''/>
                         <p>All week 24/7</p>
                     </div>
                 </div>
                 <div className='social'>
                     <a href='https://www.facebook.com/'>
-                        <img src = {Facebook} alt='' />
+                        <img src={Facebook} alt=''/>
                     </a>
                     <a href='https://twitter.com/'>
-                        <img src = {Twitter} alt='' />
+                        <img src={Twitter} alt=''/>
                     </a>
                     <a href='https://www.instagram.com/'>
-                        <img src = {Instagram} alt='' />
+                        <img src={Instagram} alt=''/>
                     </a>
                     <a href='https://www.pinterest.com/'>
-                        <img src = {Pinterest} alt='' />
+                        <img src={Pinterest} alt=''/>
                     </a>
-                </div> 
+                </div>
             </div>
             <div className="nav">
                 <div className='logo'>
@@ -53,7 +53,7 @@ function header() {
                 <div className='nav-item'>
                     <ul>
                         <li>
-                        <a href='#about'>About Us</a>
+                            <a href='#about'>About Us</a>
                         </li>
                         <li>
                             <Link to="/women"><a href='#women'>Women</a></Link>
@@ -62,33 +62,38 @@ function header() {
                             <Link to="/men"><a href='#men'>Men</a></Link>
                         </li>
                         <li>
-                        <a href='#beauty'>Beauty</a></li>
+                            <a href='#beauty'>Beauty</a></li>
                         <li>
-                        <a href='#accessories'>Accessories</a></li>
+                            <a href='#accessories'>Accessories</a></li>
                         <li>
-                        <a href='#blog'>Blog</a></li>
+                            <a href='#blog'>Blog</a></li>
                         <li>
-                        <a href='#contact'>Contact</a></li>
+                            <a href='#contact'>Contact</a></li>
                     </ul>
                 </div>
                 <div className='instryment'>
                     <a href='https://www.google.by/'>
-                        <img src = {Search} alt='' />
+                        <img src={Search} alt=''/>
                     </a>
                     <a href='https://www.google.by/'>
-                        <img src = {Global} alt='' />
+                        <img src={Global} alt=''/>
                     </a>
                     <a href='https://www.google.by/'>
-                        <img src = {User} alt='' />
+                        <img src={User} alt=''/>
                     </a>
                     <a href='https://www.google.by/'>
-                        <img src = {Shopping} alt='' />
+                        <img src={Shopping} alt=''/>
                     </a>
+                    <nav>
+                        <div className="burger_btn" onClick={() => setMenuActive(!menuActive)}>
+                            <span/>
+                        </div>
+                    </nav>
                 </div>
+
             </div>
             <div className='line'></div>
         </div>
     )
 }
 
-export default header;
