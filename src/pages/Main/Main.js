a242b45d79052454977c7aacc34e837f6dde6cca
EgorZapatylak <zapatylak96a@gmail.com>
2025-02-06 20:39:50 +0300

    Added some fixed in Main.js

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -8,28 +8,6 @@ import {Blog} from "../../components/Blog/Blog";
 import {Products} from "../../components/Products/Products";
 import {Products_base} from "../../Products_base";
 
-export const MAIN_CLOTHES_BLOCK_MENU =[
-    {
-        particularName: "isNewArrivals",
-        name: "NEW ARRIVALS"
-    },
-    {
-        particularName: "isSpecial",
-        name: "SPECIALS"
-    },
-    {
-        particularName: "isBestseller",
-        name: "BESTSELLERS"
-    },
-    {
-        particularName: "isMostViewed",
-        name: "MOST VIEWED"
-    },
-    {
-        particularName: "isFeatured",
-        name: "FEATURED PRODUCTS"
-    }
-]
 
 export default function Main() {
 
@@ -37,8 +15,8 @@ export default function Main() {
         <section className='main'>
             <Hero/>
             <Advantage/>
-            <Products title="Women's" item={Products_base.women} partic={MAIN_CLOTHES_BLOCK_MENU}/>
-            <Products title="Men's" item={Products_base.men} partic={MAIN_CLOTHES_BLOCK_MENU}/>
+            <Products title="Women's" item={Products_base.women} />
+            <Products title="Men's" item={Products_base.men} />
             <Promise/>
             <SpecialOffer/>
             <Blog/>
