a3f9b7d0c8646c54a839b6d2eae26bd62456a0f9
EgorZapatylak <zapatylak96a@gmail.com>
2025-02-17 16:37:20 +0300

    Created a component for managing the bucket status in project

---
+++ src/cartSlice.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {createSlice} from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        items: [], // Список товаров
+    },
+    reducers: {
+        addToCart: (state, action) => {
+            const item = action.payload;
+            const existingItem = store.items.find(
+                i => i.id === item.id && i.size === item.size && i.color === i.color
+            );
+
+            if (existingItem) {
+                existingItem.quantity += 1;
+            } else {
+                state.items.push({...item, quantity: 1});
+            }
+        },
+        removeFromCart:(state, action) => {
+            state.items = state.items.filter(item => item.id !==action.payload);
+        },
+    },
+});
+
+export const {addToCart, removeFromCart} = cartSlice.actions;
+export default cartSlice.reducer;
