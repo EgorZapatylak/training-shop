db5d32adfe4c1dc13f8ea799323e0c15da0a437a
EgorZapatylak <zapatylak96a@gmail.com>
2025-02-18 08:30:36 +0300

    Updated the component cartSlice.js

--- src/cartSlice.js
+++ src/cartSlice.js
@@ -20,10 +20,27 @@ const cartSlice = createSlice({
             }
         },
         removeFromCart:(state, action) => {
-            state.items = state.items.filter(item => item.id !==action.payload);
+            state.items = state.items.filter(item => item.id !== action.payload);
+        },
+        increaseQuantity: (state, action) => {
+            const item = state.items.find(i => i.id === action.payload);
+            if (item) {
+                item.quantity += 1;
+            }
+        },
+        decreaseQuantity: (state, action) => {
+            const item = state.item.find(i => i.id === action.payload);
+            if (item) {
+                if (item.quantity > 1) {
+                    item.quantity -= 1;
+                } else {
+                    //Удаляем товар, если количество становится 0
+                    state.items = state.items.filter(i => i.id !== action.payload);
+                }
+            }
         },
     },
 });
 
-export const {addToCart, removeFromCart} = cartSlice.actions;
+export const {addToCart, removeFromCart, increaseQuantity, decreaseQuantity} = cartSlice.actions;
 export default cartSlice.reducer;
