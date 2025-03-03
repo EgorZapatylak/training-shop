d563b1b95d080560b11ec5a2f1e74e6b27761090
EgorZapatylak <zapatylak96a@gmail.com>
2025-03-03 10:55:34 +0300

    Added a function for validating fields in Cart.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -278,18 +278,18 @@ export function Cart() {
         if (!address.trim()){
             newErrors.address = 'Address cannot be empty';
         }
-
-        if (paymentMethod === 'card') {
-            if (!cardNummber.match(/^\d{16}$/)) {
-                newErrors.cardNumber = 'Card number must be 16 digits.';
-            }
-            if (!expiry.match(/^(0[1-9]|1[0-2])\/\d{2}$/)) {
-                newErrors.expiry = 'Expiry must be in MM/YY format.';
-            }
-            if (!cvv.match(/^\d{3}/)) {
-                newErrors.cvv = 'Cvv must be 3 digits.';
-            }
-        }
+        //
+        // if (paymentMethod === 'card') {
+        //     if (!cardNummber.match(/^\d{16}$/)) {
+        //         newErrors.cardNumber = 'Card number must be 16 digits.';
+        //     }
+        //     if (!expiry.match(/^(0[1-9]|1[0-2])\/\d{2}$/)) {
+        //         newErrors.expiry = 'Expiry must be in MM/YY format.';
+        //     }
+        //     if (!cvv.match(/^\d{3}/)) {
+        //         newErrors.cvv = 'Cvv must be 3 digits.';
+        //     }
+        // }
         if (!isAgreed) {
             newErrors.agreement = 'You must agree to the processing of personal data.';
         }
