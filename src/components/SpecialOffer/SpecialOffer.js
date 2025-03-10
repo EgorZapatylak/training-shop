77194a1bdc9adb0d51ae2b32ddc07dbbe461605e
EgorZapatylak <zapatylak96a@gmail.com>
2025-03-10 15:51:48 +0300

    Added some fixed bugs in SpecialOffer.js

--- src/components/SpecialOffer/SpecialOffer.js
+++ src/components/SpecialOffer/SpecialOffer.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './SpecialOffer.css'
 
 export function SpecialOffer () {
+
+    const [email, setEmail] = useState('') // Состояние для email
+    const [error, setError] = useState('') // Состояние для ошибои
+
+    // Функция для валидации email
+    const validateEmail = (email) => {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; //Регулярное выражение для email
+        if (email) {
+            return 'Поле не должно быть пустым';
+        } else if (!regex.test(email)) {
+            return 'Введите корректный email';
+        }
+        return '';
+    }
+
+    // Обработчик изменений email
+    const handleEmailChange = (e) => {
+        const value = e.target.value;
+        setEmail(value);
+        setError(validateEmail(value));
+    };
+
+    // Обрвботчик отправки формы
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!error) {
+            console.log('Email валиден:', email);
+            // Действие при успешной отправке
+        }
+    };
+
+    // Проверка, валиден ли email
+    const isEmailValid = !error && email.trim() !== '';
+
     return (
         <div className='offer'>
             <div className='women_img'></div>
@@ -9,8 +43,19 @@ export function SpecialOffer () {
             <div className='offer_container'>
                 <p>Special Offer</p>
                 <span>Subscribe <br/>And Get 10% Off</span>
-                <input type='email' placeholder="Enter your email"/>
-                <button>Subscribe</button>
+                <input
+                    onSubmit={handleSubmit}
+                    type='email'
+                    placeholder="Enter your email"
+                    id='email'
+                    value={email}
+                    onChange={handleEmailChange}
+                />
+                {error && <p style={{color: 'red'}}>{error}</p>}
+                <button
+                    type='submit'
+                    disabled={!isEmailValid}
+                >Subscribe</button>
             </div>
         </div>
     );
