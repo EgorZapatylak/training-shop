d42d8f300f931115a2f3c49ac1c08ae5a25e90ec
EgorZapatylak <zapatylak96a@gmail.com>
2025-03-11 11:27:24 +0300

    Added some fixed in ReviewModal.js

--- src/components/ReviewModal/ReviewModal.js
+++ src/components/ReviewModal/ReviewModal.js
@@ -1,11 +1,27 @@
 import React, {useState} from 'react';
 import style from './ReviewModal.module.css'
 
+const StarRatingInput = ({rating, setRating}) => {
+    return(
+        <div className={style.star_rating}>
+            {[1,2,3,4,5].map((star) => (
+                <span
+                key={star}
+                className={star <= rating ? `${style.star_selected}`: `${style.star}`}
+                onClick={()=> setRating(star)}
+                >
+                    ⭐
+                </span>
+            ))}
+        </div>
+    );
+};
+
 export const ReviewModal = ({onClose, onSubmit}) => {
 
     const [name, setName] = useState('');
     const [text, setText] = useState('');
-    const [rating, setRating] = useState(5);
+    const [rating, setRating] = useState(0);
 
     const handleSubmit = () => {
         if (!name.trim() || !text.trim()) return;
@@ -20,6 +36,7 @@ export const ReviewModal = ({onClose, onSubmit}) => {
         onSubmit(newReview);
         setName('');
         setText('');
+        setRating(5);
     };
 
     return (
@@ -32,6 +49,7 @@ export const ReviewModal = ({onClose, onSubmit}) => {
                 onChange={(e)=>setName(e.target.value)}
             />
             <textarea placeholder='Your review' value={text} onChange={(e)=>setText(e.target.value)}/>
+            <StarRatingInput rating={rating} setRating={setRating}/>
             <button onClick={handleSubmit}>Send</button>
             <button onClick={onClose}>Close</button>
         </div>
