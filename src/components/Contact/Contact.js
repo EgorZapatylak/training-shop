ef3c3b34ebd70521be403efbed4f180cd732f58c
EgorZapatylak <zapatylak96a@gmail.com>
2025-03-12 14:30:04 +0300

    Added some fixed in Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {MapComponent} from "../MapComponent/MapComponent";
+import style from './Contact.module.css'
 
 export const Contact = () => {
     return (
-        <div>
+        <div className={style.container}>
             <h1>Карта с маркером</h1>
             <MapComponent />
         </div>
