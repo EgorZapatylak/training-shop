00d4ec48c005a4f56212829567d01fb1a0f230e0
EgorZapatylak <zapatylak96a@gmail.com>
2025-03-12 14:17:18 +0300

    Added some fixed in Contact.js

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Contact = () => {
+    return (
+        <div>
+            <h1>Карта с маркером</h1>
+            <MapComponent />
+        </div>
+    );
+};
