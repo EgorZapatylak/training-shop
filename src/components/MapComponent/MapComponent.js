f636c99fcf4f7d902a82ad5b0bebbd15ba339480
EgorZapatylak <zapatylak96a@gmail.com>
2025-03-12 14:17:55 +0300

    Added some fixed in MapComponent.js

---
+++ src/components/MapComponent/MapComponent.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'; // Подключи стили Leaflet
+
+export const MapComponent = () => {
+
+    const position = [52.424162, 31.014214]; // Координаты (Москва)
+
+    return (
+        <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={position}>
+                <Popup>
+                    Москва, Россия
+                </Popup>
+            </Marker>
+        </MapContainer>
+    );
+};
+
