78bb9569c7c6c7342de6cbf5360763027cdf3420
EgorZapatylak <zapatylak96a@gmail.com>
2025-03-13 08:58:33 +0300

    Added some fixed in Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import {MapComponent} from "../MapComponent/MapComponent";
 import style from './Contact.module.css'
+import Map from './img/map.png'
 
 export const Contact = () => {
     return (
         <div className={style.container}>
-            <h1>Карта с маркером</h1>
-            <MapComponent />
+            <dvi>
+                <h1>Карта с маркером</h1>
+                {/*<MapComponent/>*/}
+                <img src={Map} alt="map"/>
+            </dvi>
+            <div className={style.contacts}>
+                <div className='loc'>
+                    <p>Belarus, Gomel, Lange 17</p>
+                </div>
+                <div className='num'>
+                    <p>+375 29 100 20 30</p>
+                </div>
+                <div className='times'>
+                    <p>All week 24/7</p>
+                </div>
+                <div className='mail'>
+                    <p>info@clevertec.ru</p>
+                </div>
+            </div>
         </div>
     );
 };
