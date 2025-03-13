8d6a157bc18082fa6ee06c2fe7b1ee9005009a36
EgorZapatylak <zapatylak96a@gmail.com>
2025-03-13 15:24:55 +0300

    Updated files in project

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -4,7 +4,7 @@ import {Hero} from "../../components/Hero/Hero";
 import {Advantage} from "../../components/Advantage/Advantage";
 import {Promise} from "../../components/Promise/Promise";
 import {SpecialOffer} from "../../components/SpecialOffer/SpecialOffer";
-import {Blog} from "../../components/Blog/Blog";
+import {BlogSection} from "../../components/BlogSection/BlogSection";
 import {Products} from "../../components/Products/Products";
 import {Products_base} from "../../Products_base";
 
@@ -19,7 +19,7 @@ export default function Main() {
             <Products title="Men's" item={Products_base.men}/>
             <Promise/>
             <SpecialOffer/>
-            <Blog/>
+            <BlogSection/>
         </section>
     )
 }
