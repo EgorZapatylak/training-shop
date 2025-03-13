17b6dd75641dd6d1b892754d7182e0426999fba5
EgorZapatylak <zapatylak96a@gmail.com>
2025-03-13 16:13:42 +0300

    Added some fixed in Blog.js

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import style from './Blog.module.css'
 
-const Blog = () => {
+export const Blog = () => {
     return (
         <div>
             <h2>Последние новости</h2>
@@ -13,8 +13,22 @@ const Blog = () => {
                     <p>April 6, 2022<span>Read More</span></p>
                 </div>
             </div>
+            <div  style={{ border: '1px solid #ccc', padding: '10px', marginBottom: '10px' }}>
+                <h3>The Easiest Way to Break</h3>
+                <p>But I must explain to you how all this mistaken idea of denouncing pleas and praising
+                    pain was bor</p>
+                <div className='data'>
+                    <p>April 6, 2022<span>Read More</span></p>
+                </div>
+            </div>
+            <div  style={{ border: '1px solid #ccc', padding: '10px', marginBottom: '10px' }}>
+                <h3>The Easiest Way to Break</h3>
+                <p>But I must explain to you how all this mistaken idea of denouncing pleas and praising
+                    pain was bor</p>
+                <div className='data'>
+                    <p>April 6, 2022<span>Read More</span></p>
+                </div>
+            </div>
         </div>
     );
 };
-
-export default Blog;
