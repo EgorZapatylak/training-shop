5b4b043dbab20746b2019b48bd661c1bc8b29be0
EgorZapatylak <zapatylak96a@gmail.com>
2025-03-13 16:10:02 +0300

    Added some fixed in Blog.js

---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from './Blog.module.css'
+
+const Blog = () => {
+    return (
+        <div>
+            <h2>Последние новости</h2>
+            <div  style={{ border: '1px solid #ccc', padding: '10px', marginBottom: '10px' }}>
+                <h3>The Easiest Way to Break</h3>
+                <p>But I must explain to you how all this mistaken idea of denouncing pleas and praising
+                    pain was bor</p>
+                <div className='data'>
+                    <p>April 6, 2022<span>Read More</span></p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Blog;
