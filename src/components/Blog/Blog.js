83411a6a1210db92b4fd76d8f2367325db929af1
EgorZapatylak <zapatylak96a@gmail.com>
2025-03-17 11:51:37 +0300

    Added some fixed in Blog.js

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -7,16 +7,19 @@ export const Blog = () => {
             <h2>Последние новости</h2>
             <div className={style.container_item}>
                 <h3>The Easiest Way to Break</h3>
+                <img src="../BlogSection/img/blog_1.jpg" alt="image"/>
                 <p>But I must explain to you how all this mistaken idea of denouncing pleas and praising
                     pain was bor</p>
             </div>
             <div className={style.container_item}>
                 <h3>The Easiest Way to Break</h3>
+                <img src="../BlogSection/img/blog_2.jpg" alt="image"/>
                 <p>But I must explain to you how all this mistaken idea of denouncing pleas and praising
                     pain was bor</p>
             </div>
             <div className={style.container_item}>
                 <h3>The Easiest Way to Break</h3>
+                <img src="../BlogSection/img/blog_3.jpg" alt="image"/>
                 <p>But I must explain to you how all this mistaken idea of denouncing pleas and praising
                     pain was bor</p>
             </div>
