f928c7bd586a059ded1238b8d4eeba687149b3cf
EgorZapatylak <zapatylak96a@gmail.com>
2025-03-17 10:00:48 +0300

    Added some fixed in Blog.js

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -3,31 +3,22 @@ import style from './Blog.module.css'
 
 export const Blog = () => {
     return (
-        <div>
+        <div className={style.blog_container}>
             <h2>Последние новости</h2>
-            <div>
+            <div className={style.container_item}>
                 <h3>The Easiest Way to Break</h3>
                 <p>But I must explain to you how all this mistaken idea of denouncing pleas and praising
                     pain was bor</p>
-                <div className='data'>
-                    <p>April 6, 2022<span>Read More</span></p>
-                </div>
             </div>
-            <div>
+            <div className={style.container_item}>
                 <h3>The Easiest Way to Break</h3>
                 <p>But I must explain to you how all this mistaken idea of denouncing pleas and praising
                     pain was bor</p>
-                <div className='data'>
-                    <p>April 6, 2022<span>Read More</span></p>
-                </div>
             </div>
-            <div>
+            <div className={style.container_item}>
                 <h3>The Easiest Way to Break</h3>
                 <p>But I must explain to you how all this mistaken idea of denouncing pleas and praising
                     pain was bor</p>
-                <div className='data'>
-                    <p>April 6, 2022<span>Read More</span></p>
-                </div>
             </div>
         </div>
     );
