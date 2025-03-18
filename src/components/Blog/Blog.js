f5866a090ab74ebf72318fdc3a65ffac02fa736a
EgorZapatylak <zapatylak96a@gmail.com>
2025-03-18 08:55:45 +0300

    Added some fixed in Blog.js

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import style from './Blog.module.css'
 import BlogImages from './../BlogSection/img/blog_1.jpg'
 
 export const Blog = () => {
+
+    useEffect(() => {
+        // Прокрутка страницы к началу
+        window.scrollTo(0, 0);
+    }, []); // Пустой массив зависимостей, чтобы эффект срабатывал только при монтировани компонента
     return (
         <div className={style.blog_container}>
             <h2>Последние новости</h2>
