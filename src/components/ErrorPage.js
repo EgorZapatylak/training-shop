49f59b590a8dcaccbd9d6bfe105210da30e80c2a
EgorZapatylak <zapatylak96a@gmail.com>
2025-03-20 00:05:56 +0300

    Added some fixed in project

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import style from './ErrorPage.module.css'
 
 const ErrorPage = ({ errorCode, errorMessage }) => {
+
+    useEffect(() => {
+        // Прокрутка страницы к началу
+        window.scrollTo(0, 0);
+    }, []); // Пустой массив зависимостей, чтобы эффект срабатывал только при монтировани компонента
+
     return (
         <div className={style.container}>
             <h1 className={style.header}>{errorCode}</h1>
