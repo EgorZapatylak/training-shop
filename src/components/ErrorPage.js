47904f502a66fde9eea4974ed2682e777cbb4f3e
EgorZapatylak <zapatylak96a@gmail.com>
2025-03-19 10:02:15 +0300

    Added some fixed in ErrorPage.js

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,40 +1,14 @@
 import React from 'react';
+import style from './ErrorPage.module.css'
 
 const ErrorPage = ({ errorCode, errorMessage }) => {
     return (
-        <div style={styles.container}>
-            <h1 style={styles.header}>{errorCode}</h1>
-            <p style={styles.message}>{errorMessage}</p>
-            <a href="/" style={styles.link}>Вернуться на главную</a>
+        <div className={style.container}>
+            <h1 className={style.header}>{errorCode}</h1>
+            <p className={style.message}>{errorMessage}</p>
+            <a href="/" className={style.link}>Вернуться на главную</a>
         </div>
     );
-};
-
-const styles = {
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-        backgroundColor: '#f8f9fa',
-        color: '#343a40',
-        textAlign: 'center',
-        width: '380px',
-    },
-    header: {
-        fontSize: '6rem',
-        margin: '0',
-    },
-    message: {
-        fontSize: '1.5rem',
-        margin: '10px 0',
-    },
-    link: {
-        fontSize: '1rem',
-        color: '#007bff',
-        textDecoration: 'none',
-    },
 };
 
 export default ErrorPage;
