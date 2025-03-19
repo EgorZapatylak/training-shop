5020b34ec9759924814718add3d3cb01e8ceba27
EgorZapatylak <zapatylak96a@gmail.com>
2025-03-19 23:56:48 +0300

    Added some fixed in project

--- src/components/MapComponent/MapComponent.js
+++ src/components/MapComponent/MapComponent.js
@@ -1,23 +1,37 @@
-// import React from 'react';
-// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-// import 'leaflet/dist/leaflet.css'; // Подключи стили Leaflet
-//
-// export const MapComponent = () => {
-//
-//     const position = [52.424162, 31.014214]; // Координаты (Москва)
-//
-//     return (
-//         <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }}>
-//             <TileLayer
-//                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//             />
-//             <Marker position={position}>
-//                 <Popup>
-//                     Москва, Россия
-//                 </Popup>
-//             </Marker>
-//         </MapContainer>
-//     );
-// };
+import React, { useEffect, useState } from 'react';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 
+L.Icon.Default.mergeOptions({
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow
+});
+
+const MapComponent = () => {
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    if (!isClient) return null; // Не рендерим карту на сервере
+
+    const position = [52.424162, 31.014214];
+
+    return (
+        <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={position}>
+                <Popup>Москва, Россия</Popup>
+            </Marker>
+        </MapContainer>
+    );
+};
+
+export default MapComponent;
