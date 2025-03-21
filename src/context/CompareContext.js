5f41cdd8edd7c6943cd2214940135d1b2e54d1da
EgorZapatylak <zapatylak96a@gmail.com>
2025-03-21 09:51:07 +0300

    Added some fixed in CompareContext.js

--- src/context/CompareContext.js
+++ src/context/CompareContext.js
@@ -3,23 +3,21 @@ import React, {createContext, useContext, useState} from "react";
 export const CompareContext = createContext();
 
 export const CompareProvider = ({ children }) => {
-    const [compareItems, setCompareItems] = useState([]);
+    const [compareItems, setCompareList] = useState([]);
 
     const addToCompare = (product) => {
-        setCompareItems((prev) => {
+        setCompareList((prev) => {
             if (prev.find((item) => item.id === product.id)) return prev; // Не дублируем товары
             return [...prev, product];
         });
     };
 
     const removeFromCompare = (id) => {
-        setCompareItems((prev) => prev.filter((item) => item.id !== id));
+        setCompareList((prev) => prev.filter((item) => item.id !== id));
     };
 
-    const clearCompare = () => setCompareItems([]);
-
     return (
-        <CompareContext.Provider value={{ compareItems, addToCompare, removeFromCompare, clearCompare }}>
+        <CompareContext.Provider value={{ compareItems, addToCompare, removeFromCompare}}>
             {children}
         </CompareContext.Provider>
     );
