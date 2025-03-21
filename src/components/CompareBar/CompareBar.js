5039e68ef011c7c37a6e7fd8b4b17116a2161176
EgorZapatylak <zapatylak96a@gmail.com>
2025-03-21 11:23:31 +0300

    Added some fixed in CompareBar.js

--- src/components/CompareBar/CompareBar.js
+++ src/components/CompareBar/CompareBar.js
@@ -1,12 +1,14 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import { CompareContext } from "../../context/CompareContext";
 import "./CompareBar.css";
 
 const CompareBar = () => {
     const { compareItems, clearCompare } = useContext(CompareContext);
+    const location = useLocation();
 
-    if (compareItems.length === 0) return null; // Скрываем, если нет товаров
+    if (location.pathname === '/compare' || compareItems.length === 0) return null; // Скрываем, если нет товаров
+
 
     return (
         <div className="compare-bar">
