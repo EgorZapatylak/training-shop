923fbc5716dee778c64b2d0d391b6301caa6c026
EgorZapatylak <zapatylak96a@gmail.com>
2025-04-02 16:16:51 +0300

    Added some fixed in FavoritPage.js

--- src/components/FavoritPage/FavoritPage.js
+++ src/components/FavoritPage/FavoritPage.js
@@ -1,9 +1,11 @@
 import { useContext } from "react";
 import { FavoritesContext } from "./FavotirContext";
 import styles from "./FavoritesTab.module.css";
+import { useNavigate } from "react-router-dom";
 
 export const FavoritesTab = () => {
     const { favorites, toggleFavorite } = useContext(FavoritesContext);
+    const navigate = useNavigate();
 
     return (
         <div className={styles.favorites}>
@@ -13,11 +15,21 @@ export const FavoritesTab = () => {
             ) : (
                 <div className={styles.favoritesGrid}>
                     {favorites.map((product) => (
-                        <div key={product.id} className={styles.productCard}>
+                        <div
+                            key={product.id}
+                            className={styles.productCard}
+                            onClick={() => navigate(`/${category}/${product.id}`)}
+                        >
                             <img src={product.image} alt={product.name} />
                             <h3>{product.name}</h3>
                             <p>{product.price} руб.</p>
-                            <button onClick={() => toggleFavorite(product)}>
+                            <button
+                                className={styles.removeButton}
+                                onClick={(e) => {
+                                    e.stopPropagation(); // Предотвращает переход при нажатии на кнопку
+                                    toggleFavorite(product);
+                                }}
+                            >
                                 Удалить из избранного
                             </button>
                         </div>
